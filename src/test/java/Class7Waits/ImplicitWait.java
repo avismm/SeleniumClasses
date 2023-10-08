e4516fb9f52ac6550dec18dfc0567b1e9afc4a7a
avismm <amuhwerane@gmail.com>
2023-10-09 02:00:42 +0700

    Selenium Class 07

---
+++ src/test/java/Class7Waits/ImplicitWait.java
@@ -0,0 +1,12 @@
+package Class7Waits;
+
+import utils.CommonMethods;
+
+public class ImplicitWait extends CommonMethods {
+    public static void main(String[] args) {
+        String url="http://35.175.58.98/synchronization-waits.php";
+        String browser="chrome";
+        openBrowserAndLaunchApplication(url,browser);
+
+    }
+}
---
+++ src/test/java/utils/CommonMethods.java
@@ -0,0 +1,30 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CommonMethods {
+
+    public static  WebDriver driver;
+    public static void openBrowserAndLaunchApplication(String url, String browser){
+        //instance
+        switch (browser){
+            case "chrome":
+                driver=new ChromeDriver();
+                break;
+            case "firefox":
+                driver=new FirefoxDriver();
+                break;
+        }
+        // goto facebook.com
+        driver.get(url);
+        //        maximize the window
+        driver.manage().window().maximize();
+    }
+
+    public static void closeBrowser(){
+        driver.quit();
+    }
+
+}
